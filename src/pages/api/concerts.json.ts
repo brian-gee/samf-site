afaa5b2753eb333577a4eccf399456026cb882ac
dopecello <williamlocke.cello@gmail.com>
2023-05-16 22:59:29 -0400

    populated first event on events page level

--- src/pages/api/concerts.json.ts
+++ src/pages/api/concerts.json.ts
@@ -1,17 +1,24 @@
 export interface responseData {
-    title: string;
-    userId: number;
-    body: string;
+  title: string
+  userId: number
+  body: string
+}
+
+export interface Artist {
+  name: string
+  instrument: string | null
 }
 
 export async function get() {
-    return {
-        body: JSON.stringify([
-            {
-                "title": "Test Event",
-                "userId": 30,
-                "body": "This is a test event"
-            }, 
-        ])
-    };
+  return {
+    body: JSON.stringify([
+      {
+        title: 'Juneteenth Recognition Concert',
+        artists: [
+          { name: 'The Ritz Chamber Players'},
+          { name: 'Ann Marie McPhail', instrument: "Soprano"}
+        ],
+      },
+    ]),
+  }
 }
